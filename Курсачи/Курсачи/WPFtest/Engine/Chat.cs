6e3c439c5b454e7d6ae75441d4bd9a604132ed3b
VeNNoM <bilyalova.1972@mail.ru>
2020-06-03 19:25:09 +0300

    Курсачи

---
+++ Курсачи/Курсачи/WPFtest/Engine/Chat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFtest.Engine
+{
+   public class Chat
+    {
+        public static void SendMessage(TextBox textmessage, IPEndPoint ipPoint)
+        {
+            try
+            {
+                if (textmessage.Text != "")
+                {
+                    List<string> messagetxt = new List<string>();
+                    for (int i = 0; i < textmessage.LineCount; i++)
+                    {
+                        if (textmessage.GetLineText(i) != "")
+                        {
+                            //string s = textmessage.Lines[i].Replace("  "," ");
+                            string s = Regex.Replace(textmessage.GetLineText(i), " {2,}", " ");
+                            s = s.Trim();
+                            messagetxt.Add(s);
+                        }
+                    }
+                    //textmessage.Line = messagetxt.ToArray();                  
+                    //var id = (sender as Button).Tag;
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(ipPoint);
+                    NetworkStream nws = new NetworkStream(socket);
+                    BinaryWriter bw = new BinaryWriter(nws);
+                    BinaryReader br = new BinaryReader(nws);
+                    bw.Write($"{Properties.Settings.Default.Ulogin}");
+                    bw.Write("msg");
+                    bw.Write($"{textmessage.Tag}");
+                    bw.Write(textmessage.Text);
+                    bw.Write($"{DateTime.Now.ToUniversalTime()}");
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+
+                   
+                    textmessage.Clear();
+
+
+                }
+            }
+            catch (System.TimeoutException e1) { MessageBox.Show("Нет соединение с сервером"); }
+        }
+    }
+}
---
+++ Курсачи/Курсачи/WPFtest/Engine/Online.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WPFtest.Engine
+{
+    public class Online
+    {
+        public static  IPEndPoint IP;
+        public static TextBox Textmessage;
+        public static async Task OnlineChecker(IPEndPoint ipPoint,TextBox textmessage)
+        {
+               IP = ipPoint;
+                Textmessage = textmessage;
+                DispatcherTimer onlinetimer = new DispatcherTimer();
+                onlinetimer.Interval = new TimeSpan(0, 0, 0, 1);
+                onlinetimer.Tick += OnlineCheck;
+                onlinetimer.Start();
+            
+            //IP = ipPoint;
+            //Textmessage = textmessage;
+            //DispatcherTimer onlinetimer = new DispatcherTimer();
+            //onlinetimer.Interval = new TimeSpan(0,0,0,1);
+            //onlinetimer.Tick += OnlineCheck;
+            //onlinetimer.Start();
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //        socket.Connect(ipPoint);
+            //        NetworkStream nws = new NetworkStream(socket);
+            //        BinaryWriter bw = new BinaryWriter(nws);
+            //        BinaryReader br = new BinaryReader(nws);
+            //        bw.Write($"{Properties.Settings.Default.Ulogin}");
+            //        bw.Write($"onlinecheck");
+            //        bw.Write($"{textmessage.Tag}");
+            //        Thread.Sleep(5000);
+            //    }
+            //    catch (Exception ex) { MessageBox.Show($"{ex}"); }
+            //}
+        }
+        public async static void OnlineCheck(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+
+                 Application.Current.Dispatcher.InvokeAsync(() =>
+                {
+                    if (Textmessage.Focusable)
+                    {
+                        try
+                        {
+                            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                            socket.Connect(IP);
+                            NetworkStream nws = new NetworkStream(socket);
+                            BinaryWriter bw = new BinaryWriter(nws);
+                            BinaryReader br = new BinaryReader(nws);
+                            bw.Write($"{Properties.Settings.Default.Ulogin}");
+                            bw.Write($"onlinecheck");
+                            bw.Write($"{Textmessage.Tag}");
+                            socket.Shutdown(SocketShutdown.Both);
+                            socket.Close();
+
+                        }
+                        catch { MessageBox.Show($"Потеряно соединение с сервером, попробуйте перезайти"); Thread.Sleep(10000); }
+                    }
+                });
+               
+            });
+           
+        
+            
+        }
+    }
+}
